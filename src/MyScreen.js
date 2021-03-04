bf83314e9a17bc8ca20d1214c3d95f961ae8d638
cybanjar <cybanjar@gmail.com>
2021-03-04 16:42:20 +0700

    MyScreen Card

--- src/MyScreen.js
+++ src/MyScreen.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, Dialog, Card, Button } from 'react-native-ui-lib';
-import { StyleSheet } from 'react-native'
-
+import { TextField, View, Image, Text, Colors, Dialog, Card, Button } from 'react-native-ui-lib';
+import { StyleSheet, Alert} from 'react-native'
 
 const MyScreen = () => {
     return (
-            <View style={styles.defaultMargin} flex padding-page>
-                <Text heading marginB-s4>My Screen</Text>
-                <Card height={100} center padding-card marginB-s4>
-                    <Text body>This is an example card </Text>
+            <View style={styles.defaultMarginH} bgDark-2 flex padding-page>
+
+                <TextField style={styles.searching} placeholder="Searching..."/>
+
+                <Image style={styles.photoProfile} source={{ uri: 'http://placeimg.com/50/50/people' }}/>
+                <Card  center padding-card marginB-s4>
+                    <Image cover borderRadius={8} source={{ uri: 'http://placeimg.com/500/500/people' }} />
+                    <Text style={styles.defaultMarginV} body>This is an example card </Text>
                 </Card>
 
-                <Button label="Save" body bg-primaryColor square></Button>
+                <Button onPress={() => onSave()} label="Save" body bg-primaryColor square></Button>
             </View>
         )
     
@@ -19,9 +22,29 @@ const MyScreen = () => {
 
 export default MyScreen
 
+const onSave = () => {
+    Alert.alert('Successfully!')
+}
+
 const styles = StyleSheet.create({
-    defaultMargin: {
-        marginHorizontal: 20
+    photoProfile: {
+        width:50,
+        height: 50,
+        borderRadius: 50,
+        marginBottom: 6
+    },
+    
+    defaultMarginH: {
+        marginHorizontal: 20,
+    },
+
+    defaultMarginV: {
+        marginVertical: 16
+    },
+        
+    searching: {
+        borderStyle: 'solid',
+        marginTop: 20,
     },
     
     container: {
